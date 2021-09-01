a461b1147364b74015a261d48777f334d0ac847f
jam <jamis404@163.com>
2021-09-01 23:24:49 +0800

    按钮添加loading状态

--- src/button.vue
+++ src/button.vue
@@ -1,8 +1,9 @@
 <template>
   <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
     <g-icon v-if="icon" :name="icon"></g-icon>
+    <g-icon class="loading" name="loading"></g-icon>
     <div class="content">
-      <slot></slot>
+      <slot/>
     </div>
   </button>
 </template>
@@ -22,6 +23,10 @@ export default {
 }
 </script>
 <style lang="scss">
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
 .g-button { font-size: var(--font-size); height: var(--button-height); padding: 0 1em;
   border-radius: var(--border-radius); border: 1px solid var(--border-color);
   background: var(--button-bg);
@@ -35,6 +40,9 @@ export default {
   &.icon-right {
     > .content { order: 1; }
     > .icon { order: 2; margin-right: 0; margin-left: .1em;}
+  }
+  .loading{
+    animation: spin 2s infinite linear;
   }
 }
 </style>
