deb9cdfedfd355f4b141327db3065072d0d60c3c
jam <jamis404@163.com>
2021-09-04 14:18:21 +0800

    用chai做了第一个单元测试

--- src/app.js
+++ src/app.js
@@ -15,3 +15,19 @@ new Vue({
         loading3: false,
     }
 })
+
+import chai from 'chai'
+const expect = chai.expect
+//单元测试
+{
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData:{
+            icon: 'settings'
+        }
+    })
+    button.$mount('#test')
+    let useElement = button.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-settings')
+}
